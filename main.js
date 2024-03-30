360ddb88f647f0d9a73966385eaed8a5bbb34277
Souhrid7 <153090885+Souhrid7@users.noreply.github.com>
2024-03-30 18:29:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0
+noseY=0
+LeftWristX=0
+RightWristX=0
+difference=0
 function setup() {
     canvas=createCanvas(450,350)
     canvas.position(600,150)
@@ -12,5 +17,19 @@ function modelLoaded() {
 }
 function gotPoses(results) {
     if(results.length>0)
-    {console.log(results)}
+    {console.log(results)
+    noseX=results[0].pose.nose.x
+    noseY=results[0].pose.nose.y
+    LeftWristX=results[0].pose.leftWrist.x
+    RightWristX=results[0].pose.rightWrist.x
+    difference=floor(LeftWristX-RightWristX)
+    }
+}
+function draw() {
+    background("red")
+    fill("blue")
+    stroke("turqouise")
+    textSize(difference)
+    text("Souhrid Dasgupta",noseX,noseY)
+    document.getElementById("square_sides").innerHTML="Font Size will be "+difference+"px"
 }
